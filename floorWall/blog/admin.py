d05ea99c75805e58bf357ae040b37c68d8bca03c
Andrew <ward.andrew32@gmail.com>
2020-07-19 05:18:33 -0700

    summernotes is trash

--- floorWall/blog/admin.py
+++ floorWall/blog/admin.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post
 
-from django_summernote.admin import SummernoteModelAdmin
 
-class PostAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'slug', 'status', 'created_on')
-    list_filter = ('status', 'created_on')
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status','created_on')
+    list_filter = ("status",)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
-
-    summernote_fields = ('content',)
 
 
 @admin.register(Comment)
