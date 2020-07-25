e9b76d99df42dc0ee4213eb059015393f9eeda84
Andrew <ward.andrew32@gmail.com>
2020-07-25 02:37:39 -0700

    plz

--- floorWall/weather_or_not/urls.py
+++ floorWall/weather_or_not/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index.as_view(), name="weather_or_not"),
+   path("weather", views.index, name="weather_or_not"),
 ]
