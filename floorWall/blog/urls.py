b0970bdf31c9ffbe24f9514f31696cc7d8c798f9
Andrew <ward.andrew32@gmail.com>
2020-07-25 02:06:14 -0700

    aDFSAF

--- floorWall/blog/urls.py
+++ floorWall/blog/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path("feed/atom", AtomSiteNewsFeed()),
     path("", views.PostList.as_view(), name="home"),
     path("<slug:slug>/", views.post_detail, name="post_detail"),
-    path('weather_or_not/', include('weather_or_not.urls'), name="weather"),
 ]
